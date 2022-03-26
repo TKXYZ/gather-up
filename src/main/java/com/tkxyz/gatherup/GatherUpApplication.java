139e813a5f733be0c556a215ea1919f7010a5803
TK Tran <tktrannn@live.com>
2022-03-26 00:03:02 -0400

    First commit

---
+++ src/main/java/com/tkxyz/gatherup/GatherUpApplication.java
@@ -0,0 +1,13 @@
+package com.tkxyz.gatherup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherUpApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatherUpApplication.class, args);
+
+		System.out.println("Hello, Gather-Up!");
+	}
+}
