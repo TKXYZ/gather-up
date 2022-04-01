4becc648e289d915b49601ee18aa16757f40dba9
TK Tran <tktrannn@live.com>
2022-04-01 14:27:57 -0400

    Add Log4j2 and its XML configuration

---
+++ src/main/java/com/tkxyz/gatherup/service/UserService.java
@@ -0,0 +1,25 @@
+package com.tkxyz.gatherup.service;
+
+import com.tkxyz.gatherup.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserDao userDao;
+
+  // Log4j 2
+  private static Logger logger = LogManager.getLogger();
+
+  // CREATE
+
+  // READ
+
+  // UPDATE
+
+  // DELETE
+}
