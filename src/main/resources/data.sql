0f4ec7e22e86c2560b99bf1d0aa88b54b3c1dc06
TK Tran <tktrannn@live.com>
2022-04-01 13:46:46 -0400

    Finish User model

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
--- Populates H2 database
-INSERT INTO user (id, firstName, lastName, email, password, contact) VALUES (1, "TK", "Tran", "test@test.com", "AWDawd123", "1231231234");
+-- SQL script to prepopulate H2 database
+INSERT INTO user (id, email, password, firstName, lastName, contact) VALUES (1, "johndoe@test.com", "johndoe123", "John", "Doe", "1231231234");
+INSERT INTO user (id, email, password, firstName, lastName, contact) VALUES (2, "janedoe@test.com", "janedoe123", "Jane", "Doe", "1231231235");
