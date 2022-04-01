13ec203866fb1ce9b8e958e9c9c8c8a00bfca6c4
TK Tran <tktrannn@live.com>
2022-04-01 14:52:31 -0400

    Finish UserService

--- src/main/java/com/tkxyz/gatherup/dao/EventDao.java
+++ src/main/java/com/tkxyz/gatherup/dao/EventDao.java
@@ -1,8 +1,9 @@
 package com.tkxyz.gatherup.dao;
 
 import com.tkxyz.gatherup.model.Event;
-import org.springframework.stereotype.Repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventDao extends JpaRepository<Event, Integer> {
--- src/main/java/com/tkxyz/gatherup/service/UserService.java
+++ src/main/java/com/tkxyz/gatherup/service/UserService.java
@@ -1,10 +1,15 @@
 package com.tkxyz.gatherup.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.tkxyz.gatherup.dao.UserDao;
+import com.tkxyz.gatherup.model.User;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 @Service
 public class UserService {
@@ -13,13 +18,48 @@ public class UserService {
   private UserDao userDao;
 
   // Log4j 2
-  private static Logger logger = LogManager.getLogger();
+  private static final Logger logger = LogManager.getLogger();
 
   // CREATE
+  public User createUser(User user) {
+    if (getUserByEmail(user.getEmail()) == null) {
+      userDao.save(user);
+      logger.info("Successfully saved user: " + user.toString());
+    } else {
+      logger.error("User by that email already exists.");
+    }
+    return null;
+  }
 
   // READ
+  public List<User> getUsers() {
+    return userDao.findAll();
+  }
+
+  public User getUserById(int id) {
+    Optional<User> user = userDao.findById(id);
+    return user.get();
+  }
+
+  public User getUserByEmail(String email) {
+    List<User> userList = userDao.findAll();
+    for (User user : userList) {
+      if (user.getEmail().equals(email)) {
+        return user;
+      }
+    }
+    return null;
+  }
 
   // UPDATE
+  public User updateUser(User user) {
+    logger.info("Successfully updated user: " + user.toString());
+    return userDao.save(user);
+  }
 
   // DELETE
+  public void deleteUserById(int id) {
+    logger.info("Successfully deleted user: " + getUserById(id).getFirstName() + " " + getUserById(id).getLastName());
+    userDao.deleteById(id);
+  }
 }
