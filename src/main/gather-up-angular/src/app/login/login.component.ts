29463ce1284a46d5ed9310982034c501858a7472
TK Tran <tktrannn@live.com>
2022-06-06 21:25:26 -0400

    Start login

--- src/main/gather-up-angular/src/app/login/login.component.ts
+++ src/main/gather-up-angular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from "../class/user/user";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user: User = new User(0, "", "", "", "", "");
+
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  login() {
+
   }
 }
