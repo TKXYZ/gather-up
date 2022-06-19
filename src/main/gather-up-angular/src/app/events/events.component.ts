dc19ce5ed2985e1572549e92bf39c278a4b5961b
TK Tran <tktrannn@live.com>
2022-06-19 14:12:58 -0400

    Troubleshoot event page

--- src/main/gather-up-angular/src/app/events/events.component.ts
+++ src/main/gather-up-angular/src/app/events/events.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
 import { Event } from '../class/event/event';
 import { User } from '../class/user/user';
 import { EventService } from '../service/event/event.service';
@@ -17,24 +18,30 @@ export class EventsComponent implements OnInit {
   sessionKey: string;
   isHidden = false;
 
-  constructor(private userService: UserService, private eventService: EventService) { }
+  constructor(private loggy: NGXLogger, private userService: UserService, private eventService: EventService) { }
 
   ngOnInit(): void {
     // Extract session key from sessionStorage
     this.sessionKey = sessionStorage.getItem("email")!; // TS non-null assertion operator
+    this.loggy.warn("Session Key: " + this.sessionKey);
 
     // Validate if key exists and do something
     if (this.sessionKey == null) {
       window.location.assign("/login");
     } else {
       // GET user and their events
+      this.userService.getUserByEmail(this.sessionKey).subscribe(data => this.user = data);
+      this.eventService.getEvents().subscribe(data => this.eventList = data);
+
+      this.loggy.log(this.user);
+      this.loggy.log(this.event);
 
       // TODO: Create an eventService func that grabs event via user's ID
     }
   }
 
   saveEvent(): void {
-
+    this.loggy.log("saveEvent()");
   }
 
   deleteEvent(index: number): void {
--- src/main/gather-up-angular/src/app/profile/profile.component.ts
+++ src/main/gather-up-angular/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
 import { User } from '../class/user/user';
 import { UserService } from '../service/user/user.service';
 
@@ -12,11 +13,12 @@ export class ProfileComponent implements OnInit {
   user: User = new User(0, "", "", "", "", "");
   sessionKey: string;
 
-  constructor(private userService: UserService) { }
+  constructor(private loggy: NGXLogger, private userService: UserService) { }
 
   ngOnInit(): void {
     // Extract session key from sessionStorage
     this.sessionKey = sessionStorage.getItem("email")!; // TS non-null assertion operator
+    this.loggy.warn("Session Key: " + this.sessionKey);
 
     // Validate if key exists and do something
     if (this.sessionKey == null) {
